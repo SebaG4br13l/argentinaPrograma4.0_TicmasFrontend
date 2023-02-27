853123ca557d5fb55045348d540a138b7cd0c480
SebaG4br13l <sendrasebastiangabriel@gmail.com>
2023-02-26 23:48:06 -0300

    Subida fundamentos js1

---
+++ Fundamentos JavaScript 1/funciones.js
@@ -0,0 +1,5 @@
+
+document.getElementById('button').onclick = function() {
+    console.log('Capturamos el evento click');
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
+}
---
+++ Fundamentos JavaScript 1/modulo4 intro a JS sintaxis y como escribir un documento js.js
@@ -0,0 +1,25 @@
+// FUNDAMENTOS DE JAVASCRIPT
+
+/*
+JavaScript es un lenguaje ligero e intrepretado, orientado a objetos, mas conocido como el lenguaje de script para paginas web.
+Es el componente del desarrollo web que agrega el comportamiento de una web.
+*/
+
+//SINTAXIS
+
+//declaracion de variables
+var miVariable = "variable";
+let nombre = "nombre";
+const IVA =21;
+
+/*
+TODAS LA VARIABLES EN JS DEBEN IDENTIFICARSE CON UN NOMBRE UNICO, estos se conocen como IDENTIFICADORES.
+pueden ser cualquier nombre, pero es buena practica adoptar nombres significativos
+las reglas generales para nombrar variables son:
+    - Pueden contener letras, digitos, giones bajos y signos de dollar
+    - Deben comenzar por una letra
+    - pueden comenzar por $ o _
+    - Los nombres son case sensitive, por lo que distinguen entre matusculas y minusculas
+    - las palabras reservadas no pueden ser usadas como nombres de variables.
+*/
+
