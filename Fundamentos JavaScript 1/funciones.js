a85b0918d835bd46a354bec3b38732ab437c6687
SebaG4br13l <sendrasebastiangabriel@gmail.com>
2023-02-28 20:12:25 -0300

    actualizacion de archivos

--- Fundamentos JavaScript 1/funciones.js
+++ Fundamentos JavaScript 1/funciones.js
@@ -1,5 +1,23 @@
+//Formas de agregar comportamiento
+// para probar las funcionalidades, comentar y descomentar el bloque a probar
 
+// mediandte id
 document.getElementById('button').onclick = function() {
     console.log('Capturamos el evento click');
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
 }
+
+ 
+
+// mediante eventListener
+document.addEventListener('click',function() {
+    console.log("Hola Mundo! desde EventListener");
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
+});
+
+
+// mediante id y eventListener combinados
+document.getElementById('button').addEventListener('click', function() {
+    console.log('Capturamos el evento click');
+    document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
+});
