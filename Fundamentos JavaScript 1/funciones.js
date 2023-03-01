6159f2eeab95ca107dc48c2ca1dcb5b1bac7059f
SebaG4br13l <sendrasebastiangabriel@gmail.com>
2023-02-28 21:17:09 -0300

    parte final de fundamentos

--- Fundamentos JavaScript 1/funciones.js
+++ Fundamentos JavaScript 1/funciones.js
@@ -1,6 +1,6 @@
 //Formas de agregar comportamiento
 // para probar las funcionalidades, comentar y descomentar el bloque a probar
-
+/** 
 // mediandte id
 document.getElementById('button').onclick = function() {
     console.log('Capturamos el evento click');
@@ -15,9 +15,30 @@ document.addEventListener('click',function() {
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
 });
 
-
+*/
 // mediante id y eventListener combinados
 document.getElementById('button').addEventListener('click', function() {
     console.log('Capturamos el evento click');
     document.getElementById("demo").innerHTML = "Estamos probando nuestro primer evento en Js";
 });
+
+//cambiar colores de fondo
+
+document.getElementById('boton_color').addEventListener('click', function() {
+    //document.getElementsByTagName('body')[0].style.backgroundColor ='#FF0000'
+    document.body.style.backgroundColor ='#FF0000';
+});
+document.getElementById('boton_default').addEventListener('click', function() {
+    document.body.style.backgroundColor ='black';
+});
+
+//ocultar texto
+document.getElementById('boton_ocultar').addEventListener('click', function() {
+  document.getElementById ('demo').style.display = 'none';
+})
+
+// agregar funcionalidades mediante clases
+const collection = document.getElementsByClassName('prueba');
+for (let i = 0; i < collection.length; i++) {
+    collection[i].style.backgroundColor= 'green';
+}
